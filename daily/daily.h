4fbd7ae7d3c9e57da9b67af8fed514c9dda8fa01
Hywfred <hitwhy@gmail.com>
2020-04-21 17:26:50 +0800

    adjust project structure

---
+++ daily/daily.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef _DAILY_H
+#define _DAILY_H
+
+#include <string>
+
+class Solution {
+public:
+	int strStr(std::string haystack, std::string needle);
+};
+
+#endif // _DAILY_H
--- main.cpp
+++ main.cpp
@@ -8,14 +8,6 @@
 using namespace std;
 
 int main() {
-    vector<int> input{1, 1, 2, 1, 1};
-    Solution s;
-    cout << s.numberOfSubarrays(input, 3) << endl;
-    input = {2, 4, 6};
-    cout << s.numberOfSubarrays(input, 1) << endl;
-    input = {2, 2, 2, 1, 2, 2, 1, 2, 2, 2};
-    cout << s.numberOfSubarrays(input, 2) << endl;
-    input = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    cout << s.numberOfSubarrays(input, 3) << endl;
-    return 0;
+	cout << "hello world!" << endl;
+	return 0;
 }
