d175b731b4b870c475f764cb2bbd90fd377fb5b3
Hywfred <hitwhy@gmail.com>
2020-06-30 09:25:31 +0800

    c queue

--- daily/daily.h
+++ daily/daily.h
@@ -239,8 +239,37 @@ class Solution {
     // 数组中第 k 个最大元素
     int findKthLargest(std::vector<int> &nums, int k);
 
+    // 用两个栈实现队列 20.06.30
+    // 见类 CQueue
   private:
     long pre;
+};
+
+class CQueue {
+  public:
+    CQueue() {}
+
+    void appendTail(int value) { in_.push(value); }
+
+    int deleteHead() {
+        if (in_.empty() && out_.empty()) return -1;
+        if (out_.empty()) trans();
+        int tmp = out_.top();
+        out_.pop();
+        return tmp;
+    }
+
+  private:
+    stack<int> in_;
+    stack<int> out_;
+
+    void trans() {
+        while (!in_.empty()) {
+            int tmp = in_.top();
+            in_.pop();
+            out_.push(tmp);
+        }
+    }
 };
 
 // Your Codec object will be instantiated and called as such:
